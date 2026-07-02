c62ba452e6b752fcb5820a81c4156c22f1dbdb0c
vijay002 <vijaypatelcomp@gmail.com>
2026-07-02 20:03:13 +0530

    Add files via upload

---
+++ GetIPAddress.js
@@ -0,0 +1,11 @@
+﻿async function getIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        const ip = data.ip;
+        return ip;
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
+}
---
+++ getDeviceInformation.js
@@ -0,0 +1,19 @@
+﻿async function getDeviceInformation() {
+	// First, get the IP address
+	const ipAddress = await getIP();
+
+	const deviceInfo = {
+		deviceChannel: "Browser", // Static value, as it's coming from a browser
+		httpAcceptContent: navigator.userAgent, // Or use a more specific Accept header if needed
+		httpBrowserColorDepth: screen.colorDepth,
+		httpBrowserJavaEnabled: navigator.javaEnabled(),
+		httpBrowserJavaScriptEnabled: true, // if this code runs, JS is enabled
+		httpBrowserLanguage: navigator.language || navigator.userLanguage,
+		httpBrowserScreenHeight: screen.height,
+		httpBrowserScreenWidth: screen.width,
+		httpBrowserTimeDifference: new Date().getTimezoneOffset(), // In minutes
+		ipAddress: ipAddress, // Needs server or external service to get IP
+		userAgentBrowserValue: navigator.userAgent
+	};
+	return deviceInfo;
+}
